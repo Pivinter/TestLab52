82101facb03ccf61336fac5fe93987b176947cb9
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-10-12 16:14:26 +0300

    Add project files.

---
+++ Testlab52u/Testlab52u.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testlab52u
+{
+	TEST_CLASS(Testlab52u)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
